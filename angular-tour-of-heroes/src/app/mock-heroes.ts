1e56fd9862ff98d21b1c9e93d0e784e3010c407e
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 18:47:48 -0500

    Display Messages correctly
    Fix bug of messages appearing. Messages now appear when selecting a hero

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,6 +1,9 @@
 import { Hero } from './hero';
-
+// This is a list of the heroes and their ID numbers. This is displayed on the site
 export const HEROES: Hero[] = [
+  { id: 9, name: 'Captian America' },
+  { id: 10, name: 'Abin Sur' },
+  { id: 11, name: 'Abe Sapien' },
   { id: 12, name: 'Dr. Nice' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
@@ -9,5 +12,6 @@ export const HEROES: Hero[] = [
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr. IQ' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'A-Bomb' }
 ];
